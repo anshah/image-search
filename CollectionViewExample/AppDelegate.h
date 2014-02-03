b5e140426e1bf4cc722d7bc0e6e827f1d2bc0aa1
anshah <ankitshah_17@yahoo.com>
2014-02-03 00:29:03 -0800

    Initial Commit

---
+++ CollectionViewExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CollectionViewExample
+//
+//  Created by Ankit Nitin Shah on 2/3/14.
+//  Copyright (c) 2014 yahoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
